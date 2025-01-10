a3e600b16c35fd33d2aa9d0871343a7490caf971
Dakota Cahill <dakota.cahill@outlook.com>
2025-01-10 18:53:22 -0500

    added a rectangle as a placeholder for protag, added velocity for a jump. Needs gravity

--- dasher.cpp
+++ dasher.cpp
@@ -8,6 +8,13 @@ int main() {
 
     InitWindow(windowWidth, windowHeight, "Dapper Dasher");
 
+    // rectanlge dimensions
+    const int rectWidth{50};
+    const int rectHeight{80};    
+
+    int posY{windowHeight - rectHeight};
+    int velocity{0};
+
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
 
@@ -18,12 +25,19 @@ int main() {
         //----------------------------------------------------------------------------------
         // TODO: Update variables here
         //----------------------------------------------------------------------------------
+        posY += velocity;
 
         // Draw
         //----------------------------------------------------------------------------------
+        DrawRectangle(windowWidth / 2, posY, rectWidth, rectHeight, BLUE);
+        
+        // draw main window
         BeginDrawing();
-
         ClearBackground(RAYWHITE);
+        
+        if (IsKeyPressed(KEY_SPACE)) {
+            velocity -= 10;
+        }
 
         DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
 
