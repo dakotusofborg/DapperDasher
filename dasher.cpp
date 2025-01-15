e4bf783b04e87b331a1541e1b298f3ee99861258
Dakota Cahill <dakota.cahill@outlook.com>
2025-01-14 19:55:19 -0500

    added gravity and acceleration to jump mechanic and precluded air jumping

--- dasher.cpp
+++ dasher.cpp
@@ -7,10 +7,16 @@ int main() {
     const int windowHeight{380};
 
     InitWindow(windowWidth, windowHeight, "Dapper Dasher");
+    const int gravity{1}; // acceleration due to gravity (pixels/frame)/frame
 
     // rectanlge dimensions
     const int rectWidth{50};
-    const int rectHeight{80};    
+    const int rectHeight{80};
+
+    // is the rectangle in the air?
+    bool isInAir{};   
+    // jump velocity
+    const int jumpVel{-22}; 
 
     int posY{windowHeight - rectHeight};
     int velocity{0};
@@ -21,11 +27,6 @@ int main() {
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update variables here
-        //----------------------------------------------------------------------------------
-        posY += velocity;
 
         // Draw
         //----------------------------------------------------------------------------------
@@ -34,12 +35,30 @@ int main() {
         // draw main window
         BeginDrawing();
         ClearBackground(RAYWHITE);
+
+        // ground check - stop falling when we hit the ground
+        if (posY >= windowHeight - rectHeight)
+        {
+            // rectangle is on the ground
+            velocity = 0;
+            isInAir = false;
+        }
+        else 
+        {
+            // rectangle is in the air
+            velocity += gravity;
+            isInAir = true;
+        }
         
-        if (IsKeyPressed(KEY_SPACE)) {
-            velocity -= 10;
+        // jump check
+        if (IsKeyPressed(KEY_SPACE) && !isInAir) {
+            velocity += jumpVel;
         }
 
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        // Update
+        //----------------------------------------------------------------------------------
+        posY += velocity; // update position with new velocity
+        //----------------------------------------------------------------------------------
 
         EndDrawing();
         //----------------------------------------------------------------------------------
